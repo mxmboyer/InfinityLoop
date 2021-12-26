56b05158d08acaaf5e63ef0fe2ceed81c1e3fece
mxmboyer <mxmboyer@gmail.com>
2021-12-26 15:38:26 +0100

    struct of package components with prototype of functions to implement

--- src/main/java/components/Orientation.java
+++ src/main/java/components/Orientation.java
@@ -11,4 +11,38 @@ public enum Orientation {
 	/* Implement all the possible orientations and 
 	 *  required methods to rotate
 	 */
+	NORTH (0),
+	EAST (1),
+	SOUTH (2),
+	WEST (3);
+	
+	private final int compassDirection;
+	
+	Orientation(int compassDirection) {
+		this.compassDirection = compassDirection;
+	}
+	
+	static Orientation getOrifromValue(int orientationValue) {
+		for(Orientation o : Orientation.values()) {
+			if(o.compassDirection == orientationValue) {
+				return o;
+			}
+		}
+		return Orientation.NORTH; //à modifier : trouver + propre
+	}
+	
+	Orientation turn90() {
+		switch(this.compassDirection) {
+			case 0:
+				return Orientation.EAST;
+			case 1:
+				return Orientation.SOUTH;
+			case 2:
+				return Orientation.WEST;
+			default :
+				return Orientation.NORTH;
+		}
+		//peut mieux faire avec un truc comme : return new Orientation((this.compassDirection + 1) % 3);
+		//mais ce return en '//' ne fonctionne pas
+	}
 }
--- src/main/java/components/PieceType.java
+++ src/main/java/components/PieceType.java
@@ -11,4 +11,40 @@ import java.util.LinkedList;
  */
 public enum PieceType {
 	// Each Type has a number of connectors and a specific value
+	// PIECETYPE (number of connectors, int value),
+	VOID (0, 0),
+	ONECONN (1, 1),
+	BAR (2, 2),
+	TTYPE (3, 3),
+	FOURCONN (4, 4),
+	LTYPE (2, 5);
+
+	private final int connectors;
+	private final int intValue;
+	
+	PieceType(int connectors, int intValue) {
+		this.connectors = connectors;
+		this.intValue = intValue;
+	}
+	
+	Orientation getOrientation(Orientation orientation) {
+		// TODO
+	}
+	
+	ArrayList<Orientation> getListOfPossibleOri() {
+		//TODO
+	}
+	
+	LinkedList<Orientation> setConnectorsList(Orientation orientation) {
+		//TODO
+	}
+	
+	static PieceType getTypefromValue(int typeValue) {
+		for(PieceType pt : PieceType.values()) {
+			if(typeValue == pt.intValue) {
+				return pt;
+			}
+		}
+		return PieceType.VOID; //à modifier : trouver + propre
+	}
 }
